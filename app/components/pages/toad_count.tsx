1855a8dc22e6d5753807736242d85a17ca9f3717
Angelina Cruz <angelinacruz465@gmail.com>
2025-01-27 12:02:27 -0800

    component now reacting to page minimizing

--- app/components/pages/toad_count.tsx
+++ app/components/pages/toad_count.tsx
@@ -1,60 +1,53 @@
 import React, { useState } from "react";
-import ToadMembers from "./members"; // Import the ToadMembers component
+import ToadMembers from "./members";
 
 const ToadCountComponent: React.FC = () => {
   const [email, setEmail] = useState("");
 
   return (
-    <div
-      className="absolute left-[1215px] top-[22px] w-[271px] h-[330px] bg-[#EAFFB9] p-6 rounded-lg shadow-lg"
-    >
-      {/* Toad Count */}
-      <div className="flex flex-col items-center text-[24px] font-sunflower text-[#3C533A] -mt-2">
-        Toad Count: #
-      </div>
+    <div className="relative">
+      {/* Positioning the container closer to the top-left */}
+      <div
+        className="absolute top-2 right-2 max-w-[271px] w-full max-h-[330px] bg-[#EAFFB9] p-6 rounded-lg shadow-lg"
+      >
+        {/* Toad Count */}
+        <div className="flex flex-col items-center text-[24px] font-sunflower text-[#3C533A]">
+          Toad Count: #
+        </div>
 
-      {/* Member List */}
-      <div className="mt-3 max-h-40 overflow-y-auto scrollbar-none">
-        <div className="space-y-3">
-          {/* Can add members to the trip by calling <ToadMembers name="name" /> */}
+        {/* Member List */}
+        <div className="mt-4 max-h-40 overflow-y-auto scrollbar-none space-y-3">
           <ToadMembers name="Angelina" />
           <ToadMembers name="Billiam" />
           <ToadMembers name="Sophie" />
           <ToadMembers name="Arnav" />
           <ToadMembers name="Jiggy" />
-          <ToadMembers name="Angelina" />
         </div>
-      </div>
-      
 
-      {/* Email Input and Invite Button */}
-      {/* I believe I set the email here. use this to update invited users */}
-      <div className="flex flex-col items-center">
-        {/* Email Input */}
-        <input
-          type="email"
-          placeholder="Enter member email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="absolute top-[235px] w-[207px] h-[22px] bg-white border border-gray-300 rounded-full px-4 py-1 text-sm text-gray-800 focus:outline-none focus:ring-2 focus:ring-lime-400 text-center"
-        />
-        {/* Invite Button */}
-        <button
-          className="absolute top-[265px] w-[207px] h-[29px] bg-[#BCD3B5]/50 text-[#3C533A] rounded-lg text-sm hover:bg-[#BCD3B5]/70 text-center"
-        >
-          + Invite Member
-        </button>
-      </div>
-
-      {/* Delete Trip Button */}
-      {/* Will need to update this to delete the member and delete button on the screen and delete the member from firebase! */}
-      <div className="flex flex-col items-center">
-        <button
-          onClick={() => console.log("Trip deleted")}
-          className="absolute top-[335px] w-[271px] h-[46px] bg-[#D86D6D]/50 text-[#FFFFFF] rounded-lg text-sm hover:bg-[#D86D6D]/70 text-center"
-        >
-          Delete Trip
-        </button>
+        {/* Email Input and Invite Button */}
+        <div className="mt-2 flex flex-col items-center space-y-3">
+          <input
+            type="email"
+            placeholder="Enter member email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full max-w-[207px] h-[22px] bg-white border border-gray-300 rounded-full px-4 py-1 text-sm text-gray-800 focus:outline-none focus:ring-2 focus:ring-lime-400 text-center"
+          />
+          <button
+            className="w-full max-w-[207px] h-[29px] bg-[#BCD3B5]/50 text-[#3C533A] rounded-lg text-sm hover:bg-[#BCD3B5]/70 text-center"
+          >
+            + Invite Member
+          </button>
+        </div>
+        {/* Delete Trip Button */}
+        <div className="mt-7 flex flex-col items-center">
+            <button
+              onClick={() => console.log("Trip deleted")}
+              className="w-[271px] h-[46px] bg-[#D86D6D]/50 text-white rounded-lg text-sm hover:bg-[#D86D6D]/70 text-center"
+            >
+              Delete Trip
+            </button>
+          </div>
       </div>
     </div>
   );
