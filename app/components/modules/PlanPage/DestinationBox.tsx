5878980074a8f2fedc4e99356b71a7b428ef0b3c
Angelina Cruz <angelinacruz465@gmail.com>
2025-02-02 18:36:05 -0800

    just made template file for now and exported icons

---
+++ app/components/modules/PlanPage/DestinationBox.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MovableIcon from '/MovableIcon.svg'
+import NotCollapsed from '/NotCollapsed.svg'
+import Collapsed from '/Collapsed.svg'
+
+export default function DestinationBox() {
+    return (
+        <div className="relative w-[257px] h-[62px] bg-[#EAFFB9] rounded-lg shadow-sm"></div>
+
+    );
+
+};
+
