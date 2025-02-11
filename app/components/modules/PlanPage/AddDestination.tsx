0d04fa3b2cbc74c224dd8b6c89e55458905927eb
arnav_vishwakarma <arnavvish1@gmail.com>
2025-02-11 12:03:14 -0800

    added documentation for how to use AddDestination.tsx + an X svg

--- app/components/modules/PlanPage/AddDestination.tsx
+++ app/components/modules/PlanPage/AddDestination.tsx
@@ -1,110 +1,199 @@
-import React, {useRef} from "react"
-import { type DocumentReference } from "firebase/firestore";
+/*
+Instructions for use: 
+
+This file, when implemented, will interact with TWO other files. Sidebar, and also TripLandingPage. 
+***
+TripLandingPage: 
+***
+//import the element
+import AddDestination from '../modules/PlanPage/AddDestination';
+
+//declare this inside the function 
+const [isModalOpen, setIsModalOpen] = useState(false);
+
+// this goes in the return statement wherever 
+{isModalOpen && (
+			<AddDestination 
+			tripDbDoc={tripDbDoc}
+			onClose={() => setIsModalOpen(false)}
+			/>
+
+//wherever the sidebar is called, include code similar to this
+//the idea is that you just have to pass in the parameter "onOpenModal={() => setIsModalOpen(true)}"
+<ToadCount 
+				tripDbDoc={tripDbDoc} 
+				onOpenModal={() => setIsModalOpen(true)}
+			/>
+***
+PossibleStops.tsx
+***
+
+Inside the button element for "Add a Possible Destination", include: 
+onClick={onOpenModal}
+
+Additionally, you will have to pass in the function. So you will need to modify the function header to be something like:
+type ToadCountProps = {
+	tripDbDoc: DocumentSnapshot | null;
+	onOpenModal: () => void;
+};
+const ToadCount: React.FC<ToadCountProps> = ({ tripDbDoc, onOpenModal }) => {
+(i am using ToadCount as an example because that is where I included my button, but it will be different for PossibleStops)
+
+*/
+
+// AddDestination.tsx
+import React, { useRef, useState } from 'react';
+import { updateDoc, type DocumentSnapshot } from 'firebase/firestore';
+import cross from "/cross.svg";
 
 type AddDestinationProps = {
-    tripDbDoc: DocumentReference | null;
-    onClose: () => void;
-  };
+  tripDbDoc: DocumentSnapshot| null;
+  onClose: () => void;
+};
+
 
 const AddDestination: React.FC<AddDestinationProps> = ({ tripDbDoc, onClose }) => {
 
-    const modalContentRef = useRef<HTMLDivElement>(null);
+  const modalContentRef = useRef<HTMLDivElement>(null);
 
-    const dialogRef = useRef(null);
+  // Utility to generate a UUID (if needed)
+  function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      const r = Math.random() * 16 | 0,
+            v = c === 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
 
-    function uuidv4() {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
-        .replace(/[xy]/g, function (c) {
-            const r = Math.random() * 16 | 0, 
-                v = c == 'x' ? r : (r & 0x3 | 0x8);
-            return v.toString(16);
+  let [destinationName, setDestinationName] = useState('');
+  let [price, setPrice] = useState('');
+  let [length, setLength] = useState('');
+  let [timeOfDay, setTimeOfDay] = useState('');
+  let [description, setDescription] = useState('');
+
+
+  async function handleSubmitDestination(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if(price == null || price == "" || price == " ") {
+        price = "Free";
+    }
+    if(tripDbDoc !== null) {
+        const destKey = uuidv4();
+        const newDestination = {
+            is_in_itinerary: false,
+            name: destinationName,
+            price: price,
+            length: length,
+            time: timeOfDay,
+            description: description
+        };
+        await updateDoc(tripDbDoc.ref, {
+            [`destinations.${destKey}`]: newDestination,
         });
     }
+    onClose();
+  }
 
-    const destKey = uuidv4();
-    let dest = {}
+  const handleOverlayClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (modalContentRef.current && !modalContentRef.current.contains(e.target as Node)) {
+      onClose();
+    }
+  };
 
-    // if(props.tripDbDoc) {
-    //     props.tripDbDoc.set({destinations: {[destKey]: dest}}, { merge: true });
-    // }
-    
 
-    return(
-    // parent div; will eventually need to make the background transparent and cover the whole page
-    // <div className = "flex justify-center items-center w-full h-full p-0 bg-neutral-800/5 gap-0 mx-0 my-0">
-        <dialog ref={dialogRef} className= "flex flex-col w-3/5 justify-center items-center bg-dashboard_component_bg py-8 rounded-2xl gap-6">
-            <p className = "font-sunflower text-sidebar_deep_green text-2xl">Add A Potential Destination</p>
-            {/* Form Container */}
-            <form className = "w-full flex flex-col justify-center items-center">
-                {/* Container Div for the Destination Name form input */}
-                <div className="bg-sidebar_deep_green/15 py-4 px-8 w-11/12 rounded-2xl focus-within:ring-[#FFF]/40 focus-within:ring-2">
-                    <input
-                        type = "text"
-                        id = "destinationName"
-                        name = "destinationName"
-                        required
-                        placeholder = "Destination Name"
-                        className = "w-full min-w-32 bg-transparent text-[#FFF] placeholder:text-[#FFF]/50 font-sunflower focus:outline-none focus:ring-0  border-b-2 border-[#FFF]/50"
-                        >
-                    </input>
-                </div>
-                {/*Container Div for the 3 row inputs */}
-                <div className = "flex flex-row gap-4 w-11/12 py-4 px-4 justify-center">
-                    <div className="bg-sidebar_deep_green/15 w-1/3 py-4 px-8 rounded-2xl focus-within:ring-[#FFF]/40 focus-within:ring-2">
-                        <input
-                            type = "text"
-                            id = "price"
-                            name = "price"
-                            maxLength={14}
-                            required
-                            placeholder = "Price"
-                            className = "w-full min-w-32 bg-transparent text-[#FFF] placeholder:text-[#FFF]/50 font-sunflower focus:outline-none focus:ring-0  border-b-2 border-[#FFF]/50"
-                            >
-                        </input>
-                    </div>
-                    <div className="bg-sidebar_deep_green/15 py-4 px-8 w-1/3 rounded-2xl focus-within:ring-[#FFF]/40 focus-within:ring-2">
-                        <input
-                            type = "text"
-                            id = "length"
-                            name = "length"
-                            maxLength={14}
-                            required
-                            placeholder = "Length"
-                            className = "w-full min-w-32 bg-transparent text-[#FFF] placeholder:text-[#FFF]/50 font-sunflower focus:outline-none focus:ring-0  border-b-2 border-[#FFF]/50"
-                            >
-                        </input>
-                    </div>
-                    <div className="bg-sidebar_deep_green/15 py-4 px-8 w-1/3 rounded-2xl focus-within:ring-[#FFF]/40 focus-within:ring-2">
-                        <input
-                            type = "text"
-                            id = "timeOfDay"
-                            name = "timeOfDay"
-                            maxLength={14}
-                            required
-                            placeholder = "Time of Day"
-                            className = "w-full min-w-32 bg-transparent text-[#FFF] placeholder:text-[#FFF]/50 font-sunflower focus:outline-none focus:ring-0  border-b-2 border-[#FFF]/50"
-                            >
-                        </input>
-                    </div>
-                </div>
-                {/* Container Div for the Description */}
-                <div className="bg-sidebar_deep_green/15 py-4 px-8 w-11/12 min-h-28 rounded-2xl focus-within:ring-[#FFF]/40 focus-within:ring-2">
-                    <textarea
-                        id = "destinationName"
-                        name = "destinationName"
-                        required
-                        placeholder = "Description"
-                        className = "w-full min-w-32 min-h-36 bg-transparent text-[#FFF] placeholder:text-[#FFF]/50 font-sunflower focus:outline-none focus:ring-0"
-                        >
-                    </textarea>
-                </div>
-            </form>
-            {/* <form method = "dialog">
-                <button className = 'w-11/12 bg-sidebar_deep_green/50 font-sunflower text-[#FFF]/80 py-4 rounded-2xl'>Close</button>
-            </form> */}
-        </dialog>
-    // </div>
-    );
-}
+  return (
+    <div 
+      className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50"
+      onClick={handleOverlayClick}
+    >
+      {/* Modal content container */}
+      <div 
+        ref={modalContentRef}
+        className="flex flex-col w-2/5 justify-center items-center bg-dashboard_component_bg py-8 rounded-2xl gap-6"
+        // Stop click events from bubbling to the overlay
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className="relative flex items-center justify-center px-4 h-12">
+            <p className="font-sunflower text-sidebar_deep_green text-2xl">
+                Add a Potential Destination
+            </p>
+            <div className="absolute -right-36 rounded-full h-10 w-10 flex items-center justify-center bg-sidebar_button_bg" onClick = {onClose}>
+                <img src={cross} className="w-7 h-7"></img>
+            </div>
+        </div>
+
+
+        {/* Form Container */}
+        <form className="w-full flex flex-col justify-center items-center" onSubmit = {handleSubmitDestination}>
+          {/* Destination Name Input */}
+          <div className="bg-sidebar_deep_green/15 py-4 px-8 w-11/12 rounded-2xl focus-within:ring-[#FFF]/40 focus-within:ring-2">
+            <input
+              type="text"
+              id="destinationName"
+              name="destinationName"
+              onChange={(e) => setDestinationName(e.target.value)}
+              required
+              placeholder="Destination Name"
+              className="w-full bg-transparent text-[#FFF] placeholder:text-[#FFF]/50 font-sunflower focus:outline-none border-b-2 border-[#FFF]/50"
+            />
+          </div>
+          {/* Container Div for the three row inputs */}
+          <div className="flex flex-row gap-4 w-11/12 py-4 px-4 justify-center">
+            <div className="bg-sidebar_deep_green/15 w-1/3 py-4 px-8 rounded-2xl focus-within:ring-[#FFF]/40 focus-within:ring-2">
+              <input
+                type="text"
+                id="price"
+                name="price"
+                onChange={(e) => setPrice(e.target.value)}
+                maxLength={14}
+                placeholder="Price"
+                className="w-full bg-transparent text-[#FFF] placeholder:text-[#FFF]/50 font-sunflower focus:outline-none border-b-2 border-[#FFF]/50"
+              />
+            </div>
+            <div className="bg-sidebar_deep_green/15 py-4 px-8 w-1/3 rounded-2xl focus-within:ring-[#FFF]/40 focus-within:ring-2">
+              <input
+                type="text"
+                id="length"
+                name="length"
+                onChange={(e) => setLength(e.target.value)}
+                maxLength={14}
+                placeholder="Length"
+                className="w-full bg-transparent text-[#FFF] placeholder:text-[#FFF]/50 font-sunflower focus:outline-none border-b-2 border-[#FFF]/50"
+              />
+            </div>
+            <div className="bg-sidebar_deep_green/15 py-4 px-8 w-1/3 rounded-2xl focus-within:ring-[#FFF]/40 focus-within:ring-2">
+              <input
+                type="text"
+                id="timeOfDay"
+                name="timeOfDay"
+                onChange={(e) => setTimeOfDay(e.target.value)}
+                maxLength={14}
+                placeholder="Time of Day"
+                className="w-full bg-transparent text-[#FFF] placeholder:text-[#FFF]/50 font-sunflower focus:outline-none border-b-2 border-[#FFF]/50"
+              />
+            </div>
+          </div>
+          {/* Container Div for the Description */}
+          <div className="bg-sidebar_deep_green/15 py-4 px-8 w-11/12 min-h-28 rounded-2xl focus-within:ring-[#FFF]/40 focus-within:ring-2">
+            <textarea
+              id="description"
+              name="description"
+              placeholder="Description"
+              onChange={(e) => setDescription(e.target.value)}
+              className="w-full bg-transparent text-[#FFF] placeholder:text-[#FFF]/50 font-sunflower focus:outline-none focus:ring-0"
+            />
+          </div>
+          {/* Save/Submit Button */}
+        <button 
+            type="submit" 
+            className="gap-4 w-11/12 bg-sidebar_deep_green/50 font-sunflower text-[#FFF]/80 py-4 rounded-2xl">
+            Save
+        </button>
+        </form>
+
+      </div>
+    </div>
+  );
+};
 
 export default AddDestination;
