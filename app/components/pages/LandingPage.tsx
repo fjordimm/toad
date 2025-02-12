a6eb1fe71cd6c69fff2d31a82a646b06ccf453d8
arnav_vishwakarma <arnavvish1@gmail.com>
2025-02-11 17:42:44 -0800

    fixed LandingPage for merge conflicts

--- app/components/pages/LandingPage.tsx
+++ app/components/pages/LandingPage.tsx
@@ -12,8 +12,7 @@ export default function Landing() {
 
 	return (
 		<div className="grow flex justify-center items-center bg-dashboard_lime">
-			{/* <p className="font-sunflower text-sidebar_deep_green text-xl text-center p-5">Select a trip on the side panel to continue or create new trip!</p> */}
-			<AddDestination setIsOpen={true}></AddDestination>
+			<p className="font-sunflower text-sidebar_deep_green text-xl text-center p-5">Select a trip on the side panel to continue or create new trip!</p>
 		</div>
 	);
 }
