28c870ddf28c52d340eef8ef01e87871054025d9
fjordimm <fjordimm@gmail.com>
2025-02-12 15:44:07 -0800

    made a draggable test

--- app/components/pages/TripPagePlan.tsx
+++ app/components/pages/TripPagePlan.tsx
@@ -3,6 +3,40 @@ import { debugLogComponentRerender } from "~/src/debugUtil";
 import { useTripPageLayoutContext, type TripPageLayoutContext } from "./TripPageLayout";
 import { Link } from "react-router";
 import Itinerary from "../modules/PlanPage/Itinerary";
+import { DndContext, useDraggable, useDroppable, type DragEndEvent } from "@dnd-kit/core";
+import { useState, type ReactNode } from "react";
+
+export function MyDroppable(props: { id: string, children: ReactNode }) {
+
+	const { setNodeRef } = useDroppable({ id: props.id });
+
+	return (
+		<div ref={setNodeRef} className="bg-emerald-950 flex flex-col p-5 gap-5">
+			{props.children}
+		</div>
+	);
+}
+
+export function MyDraggable(props: { id: string, children: ReactNode }) {
+
+	const { attributes, listeners, setNodeRef, transform } = useDraggable({ id: props.id });
+
+	return (
+		<div
+			ref={setNodeRef}
+			{...listeners}
+			{...attributes}
+			className="bg-violet-300"
+			style={
+				transform
+				? { transform: `translate3d(${transform.x}px, ${transform.y}px, 0)` }
+				: undefined
+			}
+		>
+			{props.children}
+		</div>
+	);
+}
 
 export default function TripPagePlan() {
 
@@ -12,6 +46,23 @@ export default function TripPagePlan() {
 
 	const tripName = tripPageLayoutContext.tripDbDoc.get("trip_name");
 
+	const containers = ['A', 'B', 'C'];
+	const [parent, setParent] = useState<string | null>(null);
+
+	function handleDragEnd(e: DragEndEvent) {
+		const { over } = e;
+
+		if (over !== null) {
+			setParent(over.id.toString());
+		} else {
+			setParent(null);
+		}
+	}
+
+	const myDraggableElem = (
+		<MyDraggable id="draggable">Drag Me</MyDraggable>
+	);
+
 	return (
 		<div className="grow flex flex-col gap-5 bg-dashboard_lime">
 			<div className="">
@@ -19,11 +70,35 @@ export default function TripPagePlan() {
 			</div>
 
 			<div className="grow flex flex-row gap-5 justify-between">
+				<DndContext onDragEnd={handleDragEnd}>
+					<div className="bg-slate-100 flex flex-row p-5 gap-5">
+						<div className="bg-emerald-700 flex flex-col p-5 gap-5">
+							{
+								parent === null
+								? myDraggableElem
+								: null
+							}
+						</div>
+
+						{
+							containers.map((id: string) => (
+								<MyDroppable key={id} id={id}>
+									{
+										id === parent
+										? myDraggableElem
+										: <p>Drop Here</p>
+									}
+								</MyDroppable>
+							))
+						}
+					</div>
+				</DndContext>
+
 				{/* Insert Itinerary Here */}
-				<Itinerary tripDbDoc={tripPageLayoutContext.tripDbDoc}/>
+				{/* <Itinerary tripDbDoc={tripPageLayoutContext.tripDbDoc}/> */}
 
 				{/* Insert Possible Stops Column Here */}
-				<p className="bg-slate-600">Possible Stops</p>
+				{/* <p className="bg-slate-600">Possible Stops</p> */}
 			</div>
 		</div>
 	);
