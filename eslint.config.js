e3666922cf0fa5512527bfe6a63c999d4c79b64a
fjordimm <fjordimm@gmail.com>
2025-02-18 15:23:47 -0800

    turned the eslint rule 'no-explicit-any' off

--- eslint.config.js
+++ eslint.config.js
@@ -5,23 +5,27 @@ import pluginReact from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-	{
-		files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-
-	},
-	{
-		languageOptions: {
-			globals: globals.browser
-		}
-	},
-	pluginJs.configs.recommended,
-	...tseslint.configs.recommended,
-	{
-		...pluginReact.configs.flat.recommended,
-		settings: {
-			react: {
-				version: "detect"
-			}
-		}
-	}
+    {
+        files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]
+    },
+    {
+        languageOptions: {
+            globals: globals.browser
+        }
+    },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        ...pluginReact.configs.flat.recommended,
+        settings: {
+            react: {
+                version: "detect"
+            }
+        }
+    },
+    {
+        rules: {
+            "@typescript-eslint/no-explicit-any": "off"
+        }
+    }
 ];
