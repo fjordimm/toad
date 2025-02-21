8ce5dbf59bf0d3eca2923c893e8082bcbf4d99be
fjordimm <fjordimm@gmail.com>
2025-02-21 10:03:37 -0800

    started making the Expense card

---
+++ app/components/modules/TripPageExpenses/ExpenseList/Expense.tsx
@@ -0,0 +1,15 @@
+/*
+    This component is a card containing the information for a single expense.
+    It is to be used in ExpenseList.
+*/
+
+import type { DocumentSnapshot } from "firebase/firestore";
+import React from "react";
+
+export default function Expense(/*props: { tripDbDoc: DocumentSnapshot, expenseId: string }*/) {
+    return (
+        <div className="bg-toad_count_lime w-full h-96 rounded-lg">
+
+        </div>
+    );
+}
