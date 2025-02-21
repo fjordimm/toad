3a277e49be908215af550e6b7ec69b4c12a5ad43
arnav_vishwakarma <arnavvish1@gmail.com>
2025-02-20 20:46:13 -0800

    Almost done!

--- app/components/modules/TripPageExpenses/ExpenseList.tsx
+++ app/components/modules/TripPageExpenses/ExpenseList.tsx
@@ -1,14 +1,33 @@
 import React from "react";
 
-export default function ExpenseView(props: { view: "all" | "owe" | "owed" }) {
-  let displayText = "";
-  if (props.view === "all") {
-    displayText = "All Expenses";
-  } else if (props.view === "owe") {
-    displayText = "I Owe";
-  } else if (props.view === "owed") {
-    displayText = "Owed to me";
-  }
+export default function ExpenseView(props: { view: "all" | "owe" | "owed", expenses: string[], peopleOweMe: string[], iOwePeople: string[]}) {
 
-  return <p>{displayText}</p>;
+  if(props.view === "all") {
+    // return (
+    //   <>
+    //   {props.expenses.map(expense => (
+    //   <Expense uuid={expense}></Expense>
+    //   ))}
+    //   </>
+    // );
+    return(<p>All Expenses View.</p>);
+  } else if(props.view == "owe") {
+    // return (
+    //   <>
+    //   {props.iOwePeople.map(expense => (
+    //   <Expense uuid={expense}></Expense>
+    //   ))}
+    //   </>
+    // );
+    return(<p>I Owe People View.</p>);
+  } else if(props.view == "owed") {
+    // return (
+    //   <>
+    //   {props.peopleOweMe.map(expense => (
+    //   <Expense uuid={expense}></Expense>
+    //   ))}
+    //   </>
+    // );
+    return(<p>People Owe Me View.</p>);
+  }
 }
--- app/components/pages/TripPageExpenses.tsx
+++ app/components/pages/TripPageExpenses.tsx
@@ -3,11 +3,50 @@ import React, { useState } from 'react';
 import { updateDoc, type DocumentSnapshot } from 'firebase/firestore';
 import ExpenseView from '~/components/modules/TripPageExpenses/ExpenseList'
 import { useTripPageLayoutContext, type TripPageLayoutContext } from "./TripPageLayout";
-export default function BudgetPageMain(tripDbDoc: DocumentSnapshot| null) {
+export default function BudgetPageMain() {
 
     const [view, setView] = useState<"all" | "owe" | "owed">("all");
     const tripPageLayoutContext: TripPageLayoutContext = useTripPageLayoutContext();
-    const tripName = tripPageLayoutContext.tripDbDoc.get("expenses_sorted");
+    const currUser:string = tripPageLayoutContext.userDbDoc.get("email");
+    //const tripName = tripPageLayoutContext.tripDbDoc.get("expenses_sorted");
+    const expenses = tripPageLayoutContext.tripDbDoc.get("expenses");
+    const expenses_sorted: string[] = tripPageLayoutContext.tripDbDoc.get("expenses_sorted");
+    let peopleOweMe: string[] = [];
+    let iOwePeople: string[] = [];
+
+    // populating the arrays
+    for(let i = 0; i < expenses_sorted.length; i++) {
+        if(expenses[expenses_sorted[i]]["expense_owner"] === currUser) {
+        peopleOweMe.push(expenses_sorted[i]);
+        }
+        if(currUser in expenses[expenses_sorted[i]]["payers"] && expenses[expenses_sorted[i]]["expense_owner"] !== currUser) {
+        iOwePeople.push(expenses_sorted[i]);
+        }
+
+    }
+
+    // summing the values for the sidebar 
+    let toads_owe_me:number = 0;
+    let toads_paid_me:number = 0;
+    let i_owe_toads:number = 0;
+    for(let i = 0; i < iOwePeople.length; i++) {
+        if(expenses[iOwePeople[i]]["payers"][currUser][1] == 0) {
+            i_owe_toads += expenses[iOwePeople[i]]["payers"][currUser][0];
+        }
+    }
+    for(let i = 0; i < peopleOweMe.length; i++) {
+        for(const payee in expenses[peopleOweMe[i]]["payers"]) {
+            if(payee == currUser) {
+                continue;
+            }
+            if(expenses[peopleOweMe[i]]["payers"][payee][1] == 0) {
+                toads_owe_me += expenses[peopleOweMe[i]]["payers"][payee][0];
+            }
+            if(expenses[peopleOweMe[i]]["payers"][payee][1] == 1) {
+                toads_paid_me += expenses[peopleOweMe[i]]["payers"][payee][0];
+            }
+        }
+    }
 
     return(
         <div className="grow flex flex-col gap-5 bg-dashboard_lime">
@@ -20,26 +59,30 @@ export default function BudgetPageMain(tripDbDoc: DocumentSnapshot| null) {
                 <div className="grow flex flex-col w-4/5 gap-5 justify-between">
                     <div className="bg-[#D7F297] h-full rounded-xl gap-0">
                         {/* Buttons to Sort */}
-                        <div className = "flex flex-row justify-center mt-2 w-full h-9 gap-x-3">
-                            <button onClick={() => setView('all')} className = "w-1/5 rounded-lg h-full bg-black/15 hover:bg-black/35 font-sunflower text-sidebar_deep_green text-xl hover:ring-[#FFF]/40 hover:ring-2">All Expenses</button>
-                            <button onClick={() => setView('owe')} className = "w-1/5 rounded-lg h-full bg-black/15 hover:bg-black/35 font-sunflower text-sidebar_deep_green text-xl hover:ring-[#FFF]/40 hover:ring-2">I Owe</button>
-                            <button onClick={() => setView('owed')} className = "w-1/5 rounded-lg h-full bg-black/15 hover:bg-black/35 font-sunflower text-sidebar_deep_green text-xl hover:ring-[#FFF]/40 hover:ring-2">Owed to Me</button>
+                        <div className = "flex flex-row justify-center mt-2 w-full h-9">
+                            <button onClick={() => setView('all')} className = "mx-3 w-1/3 rounded-lg h-full bg-black/15 hover:bg-black/35 font-sunflower text-sidebar_deep_green text-xl hover:ring-[#FFF]/40 hover:ring-2">All Expenses</button>
+                            <button onClick={() => setView('owe')} className = "w-1/3 rounded-lg h-full bg-black/15 hover:bg-black/35 font-sunflower text-sidebar_deep_green text-xl hover:ring-[#FFF]/40 hover:ring-2">I Owe</button>
+                            <button onClick={() => setView('owed')} className = "mx-3 w-1/3 rounded-lg h-full bg-black/15 hover:bg-black/35 font-sunflower text-sidebar_deep_green text-xl hover:ring-[#FFF]/40 hover:ring-2">Owed to Me</button>
                         </div>
                         {/* By Default, display the "All Expenses" Otherwise, show the selected expenses */}
                         <div>
-                            <ExpenseView view={view}></ExpenseView>
+                            <ExpenseView view={view} expenses = {expenses_sorted} iOwePeople = {iOwePeople} peopleOweMe = {peopleOweMe}></ExpenseView>
                         </div>
                     </div>
                 </div>
                 {/* Sidebar Div */}
                 <div className="flex flex-col w-1/5 h-full gap-y-4">
                         <div className = "bg-[#D7F297] p-5 rounded-xl">
-                            <p className = "font-sunflower font-bold text-3xl text-sidebar_deep_green">You owe all toads on board</p>
-                            <p className = "font-sunflower font-bold text-3xl text-red-800">$513.26</p>
+                            <p className = "font-sunflower text-3xl text-sidebar_deep_green"><b>You owe all toads on board</b></p>
+                            <p className = "font-sunflower text-3xl text-red-800">${i_owe_toads.toFixed(2)}</p>
                         </div>
                         <div className = "bg-[#D7F297] p-5 rounded-xl">
-                            <p className = "font-sunflower font-bold text-3xl text-sidebar_deep_green">Your fellow toads owe you</p>
-                            <p className = "font-sunflower font-bold text-3xl text-red-800">$10.00</p>
+                            <p className = "font-sunflower text-3xl text-sidebar_deep_green">Your fellow toads owe you</p>
+                            <p className = "font-sunflower text-3xl text-red-800">${toads_owe_me.toFixed(2)}</p>
+                        </div>
+                        <div className = "bg-[#D7F297] p-5 rounded-xl">
+                            <p className = "font-sunflower text-3xl text-sidebar_deep_green">Your fellow toads have paid you</p>
+                            <p className = "font-sunflower text-3xl text-red-800">${toads_paid_me.toFixed(2)}</p>
                         </div>
                         <button className="bg-sidebar_deep_green rounded-xl p-2 font-sunflower text-white text-2xl">
                             Add an Expense
