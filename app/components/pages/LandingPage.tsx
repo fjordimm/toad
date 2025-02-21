3756047a4a5a53c3d75513a5f00aa793159602c7
Navodit1603 <navoditmaheshwari@gmail.com>
2025-02-20 17:32:45 -0800

    landing page acess modal

--- app/components/pages/LandingPage.tsx
+++ app/components/pages/LandingPage.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { debugLogComponentRerender } from "~/src/debugUtil";
+import NewExpense from "../modules/TripPageExpenses/NewExpense";
 
 export default function Landing() {
 
@@ -7,9 +8,19 @@ export default function Landing() {
 
     // const mainLayoutContext: MainLayoutContext = useMainLayoutContext();
 
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
     return (
         <div className="grow flex justify-center items-center bg-dashboard_lime">
             <p className="font-sunflower text-sidebar_deep_green text-xl text-center p-5">Select a trip on the side panel to continue or create new trip!</p>
+            <button onClick={() => setIsModalOpen(true)} className="relative rounded-full h-7 w-7 flex items-center justify-center bg-[#4E6A55] text-white">+</button>
+        
+
+        {
+        isModalOpen
+            ? <NewExpense onClose={() => setIsModalOpen(false)} />
+            : null
+        }   
         </div>
     );
 }
