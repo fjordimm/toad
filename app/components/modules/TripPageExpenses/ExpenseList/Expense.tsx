900dcefa2c3591b6fea59ccfdf327372da22acfe
fjordimm <fjordimm@gmail.com>
2025-02-21 10:49:41 -0800

    made it get the expenseObj and the user from the database

--- app/components/modules/TripPageExpenses/ExpenseList/Expense.tsx
+++ app/components/modules/TripPageExpenses/ExpenseList/Expense.tsx
@@ -3,13 +3,43 @@
     It is to be used in ExpenseList.
 */
 
+import React, { useEffect, useState } from "react";
 import type { DocumentSnapshot } from "firebase/firestore";
-import React from "react";
+import { dbRetrieveUser } from "~/src/databaseUtil";
+import Loading from "../../Loading";
 
 export default function Expense(props: { tripDbDoc: DocumentSnapshot, expenseId: string }) {
-    return (
-        <div className="bg-toad_count_lime w-full h-[300px] rounded-lg">
 
+    const expenseObj: any = props.tripDbDoc.get("expenses")[props.expenseId];
+
+    const [expenseOwnerDbDoc, setExpenseOwnerDbDoc] = useState<DocumentSnapshot | null>(null);
+    useEffect(
+        () => {
+            dbRetrieveUser(expenseObj.expense_owner).then(
+                (result: DocumentSnapshot) => {
+                    setExpenseOwnerDbDoc(result);
+                }
+            );
+        },
+        [props.tripDbDoc]
+    );
+
+    return (
+        <div className="bg-toad_count_lime w-full rounded-lg flex flex-col p-3 gap-3">
+            {
+                expenseOwnerDbDoc !== null
+                    ? (
+                        <div className="flex flex-row gap-3 items-center">
+                            <div className="bg-slate-700 rounded-full w-10 h-10"></div>
+                            <span className="font-sunflower text-lg">
+                                <span className="font-bold">{expenseOwnerDbDoc.get("first_name")}</span>
+                                <span> is requesting a payment split on </span>
+                                <span className="font-bold">{expenseObj.name}</span>
+                            </span>
+                        </div>
+                    )
+                    : <Loading />
+            }
         </div>
     );
 }
