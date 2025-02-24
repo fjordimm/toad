c74539281d729133e97dd069426ef2b4cccd584c
fjordimm <fjordimm@gmail.com>
2025-02-23 17:12:41 -0800

    started making the actual payers for an Expense

--- app/components/modules/TripPageExpenses/ExpenseList/Expense.tsx
+++ app/components/modules/TripPageExpenses/ExpenseList/Expense.tsx
@@ -3,7 +3,7 @@
     It is to be used in ExpenseList.
 */
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, type Dispatch, type ReactNode, type SetStateAction } from "react";
 import type { DocumentSnapshot } from "firebase/firestore";
 import { dbRetrieveUser } from "~/src/databaseUtil";
 import Loading from "../../Loading";
@@ -24,13 +24,82 @@ export default function Expense(props: { tripDbDoc: DocumentSnapshot, expenseId:
         [props.tripDbDoc]
     );
 
+    const payers: { [key: string]: any } = expenseObj.payers;
+    const payersKeys: string[] = Object.keys(payers);
+    
+    const payersDbDocs: [DocumentSnapshot | null, Dispatch<SetStateAction<DocumentSnapshot | null>>][] = [];
+    // Setting the values for payersDbDocs:
+    for (let i = 0; i < payersKeys.length; i++) {
+        payersDbDocs.push(useState<DocumentSnapshot | null>(null));
+    }
+    for (let i = 0; i < payersKeys.length; i++) {
+        useEffect(
+            () => {
+                dbRetrieveUser(payersKeys[i]).then(
+                    (result: DocumentSnapshot) => {
+                        payersDbDocs[i][1](result); // The [1] is the setter function from useState
+                    }
+                );
+            },
+            []
+        );
+    }
+
+    function turnPayersDbDocsIntoElems(): ReactNode {
+
+        const payersAsElems = [];
+        for (let i = 0; i < payersKeys.length; i++) {
+            const payerDbDoc: DocumentSnapshot | null = payersDbDocs[i][0];
+            const payerObj: any = payers[payersKeys[i]];
+
+            if (payerDbDoc !== null) {
+                payersAsElems.push(
+                    <div className="flex flex-row gap-10">
+                        <div className="w-[148px] h-[28px] bg-[#8FA789]/40 rounded-lg shadow-sm flex flex-row items-center py-1 px-2 gap-2">
+                            <div className="bg-trip_member_col_5 min-w-5 min-h-5 w-5 h-5 rounded-full"></div>
+                            <div className="overflow-hidden whitespace-nowrap text-ellipsis">
+                                <span className="text-[#3C533A] font-sunflower text-sm leading-[30px]">Howdy ho motherfucker</span>
+                            </div>
+                        </div>
+
+                        <span className="font-sunflower font-bold">hi</span>
+                    </div>
+                );
+            } else {
+                payersAsElems.push(
+                    <Loading key={i} />
+                );
+            }
+        }
+
+        return (
+            <div className="flex flex-col mx-7 gap-1">
+                {
+                    payersAsElems.map((elem: ReactNode) => {
+                        return elem;
+                    })
+                }
+            </div>
+        );
+    }
+
+    // let bruh: boolean = true;
+    // for (let i = 0; i < payersDbDocs.length; i++) {
+    //     if (payersDbDocs[i][0] === null) {
+    //         bruh = false;
+    //     }
+    // }
+    // if (bruh) {
+    //     console.log(payersDbDocs);
+    // }
+
     return (
         <div className="bg-toad_count_lime w-full rounded-lg flex flex-col p-3 gap-3">
             {
                 expenseOwnerDbDoc !== null
                     ? (
                         <div className="flex flex-row gap-3 items-center">
-                            <div className="bg-slate-700 rounded-full w-10 h-10"></div>
+                            <div className="bg-slate-700 rounded-full min-w-10 min-h-10 w-10 h-10"></div>
                             <span className="font-sunflower text-lg">
                                 <span className="font-bold">{expenseOwnerDbDoc.get("first_name")}</span>
                                 <span> is requesting a payment split on </span>
@@ -40,6 +109,7 @@ export default function Expense(props: { tripDbDoc: DocumentSnapshot, expenseId:
                     )
                     : <Loading />
             }
+            {turnPayersDbDocsIntoElems()}
         </div>
     );
 }
