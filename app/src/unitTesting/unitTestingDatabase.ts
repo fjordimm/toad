c64aa889fccf9073269018bf42dff726a6e58dad
fjordimm <fjordimm@gmail.com>
2025-03-04 16:11:49 -0800

    made the test file work on the command line

--- app/src/unitTesting/unitTestingDatabase.ts
+++ app/src/unitTesting/unitTestingDatabase.ts
@@ -1,2 +1,21 @@
+import { doc, getDoc } from "firebase/firestore";
+import { firebaseDb } from "../toadFirebase";
 
-console.log("test");
+async function main() {
+    console.log("====== Starting Test ======");
+
+    
+
+    console.log("====== Finished Test ======");
+    process.exit();
+}
+
+async function testUsers() {
+    const ret = await getDoc(doc(firebaseDb, "users", "billmularski@gmail.com"));
+    
+    if (ret.exists()) {
+        console.log(ret.id);
+    }
+}
+
+main();
